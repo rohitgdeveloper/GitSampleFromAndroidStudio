0fa5c889711d5e94b54aafa8032ff86c48395d77
Rohit <rohitg.developer@gmail.com>
2016-10-24 00:24:11 +0530

    Comment is Added 2

--- app/src/main/java/com/gitsamplefromandroidstudio/MainActivity.java
+++ app/src/main/java/com/gitsamplefromandroidstudio/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Add comment in First Change in project
+        //Add 2nd comment
     }
 }
